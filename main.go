d8eb8e0d070b27d1101fe73ee1932716625ab50a
Buse Duran <buseduran500@gmail.com>
2024-03-31 15:29:57 +0300

    variables

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt" //io package
 
 func main() {
-	fmt.Print("Hello World")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50 //cannot change like js
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
 }
