3e7d3a5f5490e7a08c9e7d4844ebc92dc1ff5ccc
Buse Duran <buseduran500@gmail.com>
2024-04-09 23:55:50 +0300

    switch statement

--- main.go
+++ main.go
@@ -72,8 +72,27 @@ func main() {
 			}
 
 		} else {
+			if !isValidName {
+				fmt.Println("first name or last name you entered is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("email address you entered doesn't contain @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("number if tickets you entered is invalid")
+			}
 			fmt.Println("Your input data is invalid, try again.")
 		}
+	}
 
+	city := "London"
+
+	switch city {
+	case "New York":
+	case "Singapore", "Hong Kong":
+	case "Berlin", "London":
+	case "Mexico":
+	default:
+		fmt.Print("No valid city selected")
 	}
 }
