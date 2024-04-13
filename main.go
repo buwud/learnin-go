46831adac6b09e5dfc27f206d9da2851db0c7b75
Buse Duran <buseduran500@gmail.com>
2024-04-13 22:34:38 +0300

    functioning

--- main.go
+++ main.go
@@ -5,52 +5,32 @@ import (
 	"strings"
 )
 
+// Package Level Variables - Clean Code
+const conferenceTickets int = 50     //cannot change like js
+var conferenceName = "Go Conference" //string, create var w this syntax but cannot define type explicitly
+var remainingTickets uint = 50       //int
+// uint -> positive whole numbers
+var bookings []string //array type
+
 func main() {
 	//syntactic sugar -> describe a feauture that lets you do smth more easily
-	conferenceName := "Go Conference" //string, create var w this syntax but cannot define type explicitly
-	const conferenceTickets int = 50  //cannot change like js
-	var remainingTickets uint = 50    //int
-	//uint -> positive whole numbers
-	var bookings []string //array type
-
-	greetUsers(conferenceName, conferenceTickets, remainingTickets)
+	greetUsers()
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T \n", conferenceTickets, remainingTickets, conferenceName)
 	//%T used to print data-type
 
 	//only have for loop FOR loop c:
 	for {
-		var firstName string //go doesnt know its type because its not assigned initially, have to add type
-		var lastName string
-		var email string
-		var userTickets uint
-		//ask user for their name
-		fmt.Print(("Enter your first name: "))
-		fmt.Scan(&firstName)
 
-		fmt.Print(("Enter your last name: "))
-		fmt.Scan(&lastName)
-
-		fmt.Print(("Enter your email address: "))
-		fmt.Scan(&email)
-
-		fmt.Print(("Enter number of tickets: "))
-		fmt.Scan(&userTickets)
-
+		firstName, lastName, email, userTickets := getUserInput()
 		//isValidCity := city == "Singapore" || city == "London"
-		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
 
 		if isValidName && isValidEmail && isValidTicketNumber {
-			remainingTickets = remainingTickets - userTickets
-			// bookings[0] = firstName + " " + lastName
-			bookings = append(bookings, firstName+" "+lastName)
-			fmt.Printf("The first value: %v\n", bookings[0])
 
-			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-
+			bookTicket(userTickets, firstName, lastName, email)
 			//call func
-			firstNames := getFirstNames(bookings)
+			firstNames := getFirstNames()
 			fmt.Printf("The first names of bookings are: %v \n", firstNames)
 
 			// noTicketRemaining bool = remainingTickets == 0
@@ -87,12 +67,12 @@ func main() {
 	}
 }
 
-func greetUsers(confName string, confTickets int, remainingTickets uint) { //only executed when called
-	fmt.Printf("Welcome to %v booking application\n", confName)
-	fmt.Printf("We have total of %v tickets and %v are still available\n", confTickets, remainingTickets)
+func greetUsers() { //only executed when called
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 }
-func getFirstNames(bookings []string) []string {
+func getFirstNames() []string {
 	firstNames := []string{}
 	//blank identifier (_) -> to ignore a variable you dont want to use
 	for _, booking := range bookings {
@@ -103,9 +83,39 @@ func getFirstNames(bookings []string) []string {
 	return firstNames
 	//fmt.Printf("The first names of bookings are: %v \n", firstNames)
 }
-func validateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
+func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
 	isValidName := len(firstName) >= 2 && len(lastName) >= 2 //validate -> at least 2 chars
 	isValidEmail := strings.Contains(email, "@")
 	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 	return isValidName, isValidEmail, isValidTicketNumber
+}
+
+func getUserInput() (string, string, string, uint) {
+	var firstName string //go doesnt know its type because its not assigned initially, have to add type
+	var lastName string
+	var email string
+	var userTickets uint
+	//ask user for their name
+	fmt.Print(("Enter your first name: "))
+	fmt.Scan(&firstName)
+
+	fmt.Print(("Enter your last name: "))
+	fmt.Scan(&lastName)
+
+	fmt.Print(("Enter your email address: "))
+	fmt.Scan(&email)
+
+	fmt.Print(("Enter number of tickets: "))
+	fmt.Scan(&userTickets)
+	return firstName, lastName, email, userTickets
+}
+
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
+	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+	bookings = append(bookings, firstName+" "+lastName)
+	//fmt.Printf("The first value: %v\n", bookings[0])
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
