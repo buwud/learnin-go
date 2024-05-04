f8c75fec0b4fd6fc53673cd40fba13712e24bd51
Buse Duran <buseduran500@gmail.com>
2024-05-04 23:54:57 +0300

    math random

--- greetings/greetings/greetings.go
+++ greetings/greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
@@ -11,7 +12,18 @@ func Hello(name string) (string, error) {
 		return "", errors.New("empty name")
 	}
 
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 	//nil means no error
+}
+
+// randomly return a string from slice
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Hello beautiful %v!",
+		"Hail, %v. Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
