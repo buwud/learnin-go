c41770a00639987528975893597ef43dff777a72
Buse Duran <buseduran500@gmail.com>
2024-05-15 00:39:33 +0300

    ✨ feat: add function to retrieve albums by artist

--- 4_relDb/data-access/main.go
+++ 4_relDb/data-access/main.go
@@ -10,6 +10,13 @@ import (
 
 var db *sql.DB
 
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
 var userName string
 var password string
 
@@ -44,4 +51,35 @@ func main() {
 		log.Fatal(pingErr)
 	}
 	fmt.Println("connected <3")
+
+	//call getAlbumsByArtist
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
+}
+
+// albumsByArtist query
+func albumsByArtist(name string) ([]Album, error) {
+	var albums []Album //albums slice
+
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name) //get artist's rows
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var alb Album
+		//Scan takes a list of pointers to Go values, where the column values will be written.
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	//After the loop, check for an error from the overall query, using rows.Err.
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	return albums, nil
 }
